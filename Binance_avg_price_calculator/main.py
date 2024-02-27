a437f0774607d30090b640490dcdcfe31a851c36
mathewae <110407060+mathewae@users.noreply.github.com>
2024-02-27 13:19:33 +0100

    Update main.py

--- Binance_avg_price_calculator/main.py
+++ Binance_avg_price_calculator/main.py
@@ -18,14 +18,3 @@ side = "BUY"
 days_back = 1
 
 get_average_price_futures(asset, side, days_back)
-
-# url = "https://api.hyperliquid.xyz/info"
-# headers = {"accept": "application/json"}
-#
-# data_pos = {
-#     "type": "userFills",
-#     "user": "0x1939286838F986871e7A2d24253A8d01a7c2528e",
-# }
-#
-# response = requests.post(url, json=data_pos, headers=headers)
-# print(json.dumps(response.json(), indent=4))
